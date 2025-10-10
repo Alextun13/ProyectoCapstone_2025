cc9bf25f51f44e1d7c28dcd2033bbf4b5fcb2563
Alexander Lucero <142625103+Alextun13@users.noreply.github.com>
2025-10-09 23:33:07 -0300

    Delete 004D/Equipo 11/Fase 2/capacitor.config.ts

--- 004D/Equipo 11/Fase 2/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'firemaps',
-  webDir: 'www'
-};
-
-export default config;
